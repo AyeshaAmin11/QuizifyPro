7cec179fc3751159f47a2d1507f6cfff4c983ad6
Ayesha Amin <ayeshaameen257@gmail.com>
2025-07-21 09:23:44 +0500

    second commit

--- WebQuiz/settings.py
+++ WebQuiz/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY='django-insecure-n-%v0fp2#4k#$vsz87t1b%2@=a(nz)3%lj1ps4eet9*cg&#+yi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
